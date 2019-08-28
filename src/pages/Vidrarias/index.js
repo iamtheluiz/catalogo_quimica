2b87de2dc655e422eb23475026e70865b70350bf
iamtheluiz <iamtheluiz.dev@gmail.com>
2019-08-27 22:50:42 -0300

    Adicionadas todas as páginas do site

---
+++ src/pages/Vidrarias/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Styles
+import './styles.css';
+
+export default function Simbologia() {
+  return(
+    <div className="Simbologia">
+      
+    </div>
+  )
+}
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,22 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 // Pages
 import Home from './pages/Home';
+import Normas from './pages/Normas';
+import Vidrarias from './pages/Vidrarias';
+import Equipamentos from './pages/Equipamentos';
+import Simbologia from './pages/Simbologia';
+import Referencias from './pages/Referencias';
 
 export default function Routes() {
   return(
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/normas" exact component={Normas} />
+        <Route path="/vidrarias" exact component={Vidrarias} />
+        <Route path="/equipamentos" exact component={Equipamentos} />
+        <Route path="/simbologia" exact component={Simbologia} />
+        <Route path="/referencias" exact component={Referencias} />
       </Switch>
     </BrowserRouter>
   )
