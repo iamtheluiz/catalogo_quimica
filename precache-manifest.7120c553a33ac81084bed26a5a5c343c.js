43bd975b20620c05927669ab0ecb203ce0d19cba
iamtheluiz <iamtheluiz.dev@gmail.com>
2019-08-28 15:34:57 -0300

    Updates

---
+++ precache-manifest.7120c553a33ac81084bed26a5a5c343c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a04fe6945777a6be48f194c15f19c3",
+    "url": "/catalogo_quimica/index.html"
+  },
+  {
+    "revision": "935d9c1865ddbca3ecac",
+    "url": "/catalogo_quimica/static/css/main.d11f948b.chunk.css"
+  },
+  {
+    "revision": "6af73f0ee80adf691a69",
+    "url": "/catalogo_quimica/static/js/2.a810148d.chunk.js"
+  },
+  {
+    "revision": "935d9c1865ddbca3ecac",
+    "url": "/catalogo_quimica/static/js/main.aad3345b.chunk.js"
+  },
+  {
+    "revision": "206b5b87d1df78f63597",
+    "url": "/catalogo_quimica/static/js/runtime~main.df348392.js"
+  },
+  {
+    "revision": "0b04478a610a4151e545b87f879ffe89",
+    "url": "/catalogo_quimica/static/media/home.0b04478a.svg"
+  },
+  {
+    "revision": "19758468c168f4605620846c9b5c8a00",
+    "url": "/catalogo_quimica/static/media/link.19758468.svg"
+  },
+  {
+    "revision": "eec7c7f60134e712ef7174c96ca7ee5a",
+    "url": "/catalogo_quimica/static/media/logo.eec7c7f6.svg"
+  }
+]);
