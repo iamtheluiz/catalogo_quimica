ee9c9c64e061eaa330d4e00aa27d535a861e2593
iamtheluiz <iamtheluiz.dev@gmail.com>
2019-08-27 22:33:51 -0300

    Adicionados links para os tópicos

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // Page Style
 import './styles.css';
 
-// Logo
-import logo from '../../logo.svg';
+// Svg's
+import logo from '../../assets/logo.svg';
+import link from '../../assets/link.svg';
 
 // Components
 import Integrantes from '../../components/Integrantes';
 import Footer from '../../components/Footer';
 
+// Tópicos do Site
+import topicos from '../../services/topicos';
+
 // Page Component
 export default function Home() {
-  return(
+  return (
     <div className="Home">
       <header>
         <img id="logo" src={logo} alt="Website Logo" />
@@ -24,6 +29,13 @@ export default function Home() {
         <h2>O Projeto</h2>
         <p>Esse projeto foi desenvolvido como atividade da matéria "Laboratório de Práticas Integradas 1B", ministrada pela Professora Cristina Porto no período 2019/2.</p>
         <p>A principal ideia de desenvolver um site acerca desse conteúdo é a contecentração dos tópicos em uma única plataforma, para que não seja necessária uma pesquisa excessiva em vários sites ao necessitar de resposta para algumas perguntas.</p>
+        <h2>Conteúdo</h2>
+        <ul>
+          {topicos.map((topico, index) => (
+            <li key={index}><img className="linkSvg" src={link} alt="Anchor" /><Link to={topico.url}>{topico.nome}</Link></li>
+          ))}
+        </ul>
+        <p className="legenda">* Obs: Para acessar um tópico, clique no link</p>
       </main>
       <Integrantes />
       <Footer />
---
+++ src/services/topicos.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    nome: "Normas de Segurança",
+    url: "/normas"
+  },
+  {
+    nome: "Vidrarias",
+    url: "/vidrarias"
+  },
+  {
+    nome: "Equipamentos",
+    url: "/equipamentos"
+  },
+  {
+    nome: "Simbologia de Risco",
+    url: "/simbologia"
+  },
+  {
+    nome: "Referências",
+    url: "/referencias"
+  },
+]
