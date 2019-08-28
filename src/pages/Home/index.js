114b79f96a682edff2d1ea2e449135ab260aeda8
iamtheluiz <iamtheluiz.dev@gmail.com>
2019-08-27 22:11:15 -0300

    Adicionado botão para scroll

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -18,9 +18,12 @@ export default function Home() {
         <img id="logo" src={logo} alt="Website Logo" />
         <h1>Catálogo de Química</h1>
         <span>Um manual de boas práticas e conhecimentos para laboratórios de química!</span>
+        <a id="scrollLink" href="#projetoMain">+</a>
       </header>
-      <main>
-        <p>auisdhas iduah uidahsuidh asudh aisuhdai ushd auisdhasi</p>
+      <main id="projetoMain">
+        <h2>O Projeto</h2>
+        <p>Esse projeto foi desenvolvido como atividade da matéria "Laboratório de Práticas Integradas 1B", ministrada pela Professora Cristina Porto no período 2019/2.</p>
+        <p>A principal ideia de desenvolver um site acerca desse conteúdo é a contecentração dos tópicos em uma única plataforma, para que não seja necessária uma pesquisa excessiva em vários sites ao necessitar de resposta para algumas perguntas.</p>
       </main>
       <Integrantes />
       <Footer />
